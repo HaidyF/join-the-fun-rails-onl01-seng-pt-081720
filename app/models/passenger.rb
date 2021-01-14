1e311770823ae89c99611addd03d67b10248596c
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-01-14 17:05:38 +0200

    Done.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,18 @@
 class Passenger < ActiveRecord::Base
+    has_many :rides
 end
+
+# class Post
+#     has_many :posts_tags
+#     has_many :tags, through: :posts_tags
+#   end
+   
+#   class PostsTag
+#     belongs_to :post
+#     belongs_to :tag
+#   end
+   
+#   class Tag
+#     has_many :posts_tags
+#     has_many :posts, through: :posts_tags
+#   end
